2853b504089af8eaca9fa44b28f3e147c21dfec2
Kelvin Kellner <kelvinkellner123@gmail.com>
2019-11-30 15:32:59 +0000

    Changes comments in code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # Imports
 from random import choice, randint
 
-heaps = [3,4,5]
-
 YES_PHRASES = ["y","yes","sure","ok","okay","yes please","alright","bet"]
 NO_PHRASES = ["n","no","nah","nahhh","nope","no thank you"]
 AFFIRMATION = ["perfect","awesome","fantastic","amazing","nice","great","okie dokie","sounds good","okay","solid","cool, cool","alrighty","magnificent","excellent","tiiight"]
@@ -20,7 +18,7 @@ def affirm():
   print(affirmative)
   return affirmative
 
-# One turn for a given player
+# Plays through one turn for a given player
 def turn(name):
   print("\n\n\n{:s}'s turn!\n".format(name))
 
@@ -51,6 +49,7 @@ def turn(name):
     print("\n\n\nGAME OVER.\n{:s} has lost the game!\n\n".format(name))
     return True
 
+# Does all the magic to start and run a new game
 def new_game():
   print("\n\nWelcome to Nim!\nThis is a two-player thinking game coded using Python.")
   print("\n\nHow this game works:\nEither player takes turns taking 1-3 coins from any one heap of their choosing.\nWhichever player takes the last coin loses the game.")
@@ -70,9 +69,10 @@ def new_game():
   affirm()
 
   
-
+# Start the game
 new_game()
 
+heaps = [3,4,5]
 players = ["Kelvin","Marco","Nicole"]
 
 over = False
