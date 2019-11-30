35dc4cf55baa7f3eb8b33e84f48ab0b5ff6db204
Kelvin Kellner <kelvinkellner123@gmail.com>
2019-11-30 15:53:33 +0000

    Setup custom heap setup option.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Imports
 from random import choice, randint
 
+# Constants for input/output, so the game feels smarter and more sentient, lol.
 YES_PHRASES = ["y","yes","sure","ok","okay","yes please","alright","bet"]
 NO_PHRASES = ["n","no","nah","nahhh","nope","no thank you"]
 AFFIRMATION = ["perfect","awesome","fantastic","amazing","nice","great","okie dokie","sounds good","okay","solid","cool, cool","alrighty","magnificent","excellent","tiiight"]
@@ -11,29 +12,29 @@ def print_heaps(heaps):
     print("Heap {:d} - {:s}".format(i+1, "|"*heaps[i]))
   return
 
-# Adds a phrase of affirmation, followed by an '!'
+# Adds a phrase of affirmation, followed by a '.'
 def affirm():
   word_choice = choice(AFFIRMATION)
-  affirmative = "{}.".format(word_choice[0].upper() + word_choice[1:].lower())
+  affirmative = "{:s}.".format(word_choice[0].upper() + word_choice[1:].lower())
   print(affirmative)
   return affirmative
 
 # Plays through one turn for a given player
-def turn(name):
+def turn(name, heaps):
   print("\n\n\n{:s}'s turn!\n".format(name))
 
   print_heaps(heaps)
 
   move = ""
-  while (not move.isdigit()) or int(move) > len(heaps):
-    print("\nWhich heap will you take from?")
+  while not move.isdigit() or int(move) > len(heaps):
+    print("\nWhich Heap will you take from?")
     move = input(" > ")
   taking_from = int(move)-1
   affirm()
 
   taking = ""
-  while (not taking.isdigit()) or int(taking) < 1:
-    print("\nHow many coins would you like to take from heap {}?".format(move))
+  while not taking.isdigit() or int(taking) < 1:
+    print("\nHow many coins would you like to take from Heap {}?".format(move))
     taking = input(" > ")
   affirm()
 
@@ -42,6 +43,7 @@ def turn(name):
   total = 0
   for heap in heaps:
     total += heap
+  print(total)
   if total > 0:
     print("\nYour turn is over.")
     return False
@@ -52,7 +54,7 @@ def turn(name):
 # Does all the magic to start and run a new game
 def new_game():
   print("\n\nWelcome to Nim!\nThis is a two-player thinking game coded using Python.")
-  print("\n\nHow this game works:\nEither player takes turns taking 1-3 coins from any one heap of their choosing.\nWhichever player takes the last coin loses the game.")
+  print("\n\nHow this game works:\nEither player takes turns taking 1-3 coins from any one Heap of their choosing.\nWhichever player takes the last coin loses the game.")
   
   said = ""
   while said not in YES_PHRASES and said not in NO_PHRASES:
@@ -63,20 +65,42 @@ def new_game():
 
   said = ""
   while said not in YES_PHRASES and said not in NO_PHRASES:
-    print("\n\nWould you like to customize the heap setup?")
+    print("\n\nWould you like to customize the Heap setup?")
     said = input(" > ").lower()
   custom_game = said in YES_PHRASES
   affirm()
 
+  if custom_game:
+    heaps = []
+    said = ""
+    while not said.isdigit() or int(said) < 1:
+      print("\n\nHow many Heaps would you like to play with (default is 3)?")
+      said = input(" > ")
+    for i in range(int(said)):
+      said = ""
+      while not said.isdigit() or int(said) < 1:
+        print("\n\nHow many coins would you like in Heap {:d}?".format(i+1))
+        said = input(" > ")
+      heaps.append(int(said))
+      affirm()
+    print("\n\n{:s}\nWe are all done now.\n\nSo let's play some Nim!".format(affirm()))
+  else:
+    heaps = [3,4,5]
+    affirm()
+
+  players = ["Kelvin","Marco","Nicole"]
+
+  # Loop through turns until the game is over
+  over = False
+  if bot:
+    ""
+  else:
+    while not over:
+      for p in players:
+        if not over:
+          over = turn(p, heaps)
+
+
   
 # Start the game
 new_game()
-
-heaps = [3,4,5]
-players = ["Kelvin","Marco","Nicole"]
-
-over = False
-while not over:
-  for p in players:
-    over = turn(p)
-
